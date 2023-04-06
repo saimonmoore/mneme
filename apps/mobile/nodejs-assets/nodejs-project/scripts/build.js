17556e4b9c491d33b82fd142f0be47db0b079fb8
Saimon Moore <saimonmoore@gmail.com>
2023-04-06 22:05:57 +0200

    remap sodium-native in package.json

---
+++ apps/mobile/nodejs-assets/nodejs-project/scripts/build.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function reMapSodiumNative() {
+  const packagesToRemap = [
+    'sodium-universal',
+    'sodium-secretstream',
+    'hmac-blake2b',
+  ];
+
+  packagesToRemap.forEach(packageName => {
+    const pathToPackageJSON = `../node_modules/${packageName}/package.json`;
+    const packageJSON = require(pathToPackageJSON);
+
+    const browser = packageJSON.browser || {};
+    browser['sodium-native'] = 'sodium-native-nodejs-mobile';
+
+    packageJSON.browser = browser;
+
+    fs.writeFile(
+      `${__dirname}/${pathToPackageJSON}`,
+      JSON.stringify(packageJSON, null, 2),
+      function writeJSON(err) {
+        if (err) {
+          return console.log(err);
+        }
+
+        console.log(
+          'remapped sodium-native to mobile version for "' + packageName + '"',
+        );
+      },
+    );
+  });
+}
+
+reMapSodiumNative();
